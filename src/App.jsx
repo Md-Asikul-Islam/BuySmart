e39bf8e508c8d519ff2cb796545765df2a838332
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-20 20:24:34 +0600

    add TopBanner component with newsletter subscription form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from './Components/Header'
+import TopBanner from './Components/TopBanner'
+import NewsLetter from './Components/NewsLetter'
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div>
+      <TopBanner />
       <Header />
+      <NewsLetter />
+      <Footer />
     </div>
   )
 }
