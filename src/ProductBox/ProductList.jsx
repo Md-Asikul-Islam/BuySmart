1e99fd87cb16d87f7b4e6a46c36f79b5146c48b2
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-20 20:34:42 +0600

    implement ProductList component to display products using ProductCard

--- src/ProductBox/ProductList.jsx
+++ src/ProductBox/ProductList.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import ProductCard from "./ProductCard";
+import ProductListHeader from "./ProductListHeader";
+import { getProducts } from "../data/products";
 
 const ProductList = () => {
+  const products = getProducts();
   return (
-    <div>ProductList</div>
-  )
-}
+    <div className="lg:col-span-2">
 
-export default ProductList
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
