020a566cdc2d0aa9bc75f85a4edbbc2d3c62f72f
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-20 20:43:10 +0600

    adding component ProductList and Cart in APP

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ const App = () => {
     <div>
       <TopBanner />
       <Header />
+      <main className="container mx-auto px-4 md:px-8 py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <ProductList />
+          <Cart />
+        </div>
+      </main>
       <NewsLetter />
       <Footer />
     </div>
