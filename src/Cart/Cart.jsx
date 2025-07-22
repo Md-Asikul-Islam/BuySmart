0ee78d928ae42a2eac98df7538697775fdadd9c1
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-22 23:08:18 +0600

    create a state and passing data cart item

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,15 +1,18 @@
-import React from "react";
-import CartItem from "./CartItem";
+import  { useContext } from "react";
+import CartItem from "./CartItem"
 import OrderSummary from "./OrderSummary";
 import SearchAndSort from "./SearchAndSort";
+import { CartContext } from "../Context";
+
 
 
 const Cart = () => {
+  const {cartItem} = useContext(CartContext)
   return (
     <div className="lg:col-span-1">
       <div className="bg-white rounded-lg p-6 border border-gray-200">
         <h2 className="text-2xl font-bold mb-6">YOUR CART</h2>
-        <CartItem />
+        {cartItem.map((item) => <CartItem key={item.id} item={item} />  )}
         <OrderSummary />
         <SearchAndSort />
       </div>
--- src/Context/CartProvider.jsx
+++ src/Context/CartProvider.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import { CartContext } from "./index";
 
 export const CartProvider = ({ children }) => {
+
+  const [cartItem, setCartItem] = useState([]);
+
+
+  // check item is in cart 
+  const isInCart() => {
+
+  }
+
   return (
-  <CartContext.Provider value={{}} >
+  <CartContext.Provider value={{isInCart, cartItem}} >
     {children}
   </CartContext.Provider>
 );
