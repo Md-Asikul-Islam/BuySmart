042934d367a0702744f7e71a32d20e6ef2a3f201
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-23 23:23:39 +0600

    checking the condition and improve the code

--- src/Cart/CartItem.jsx
+++ src/Cart/CartItem.jsx
@@ -3,7 +3,7 @@ import { getImageUrl } from "../Utils/utility";
 import { CartContext } from "../Context";
 
  const CartItem = ({item}) => {
-  const {removeFromCart} = useContext(CartContext)
+  const {removeFromCart, increaseItem, decreaseItem} = useContext(CartContext)
   return (
     <div className="flex items-start space-x-4 pb-4 border-b border-gray-200 mb-4">
       <div className="w-16 h-16 bg-gray-100 rounded flex-shrink-0 flex items-center justify-center">
@@ -23,13 +23,17 @@ import { CartContext } from "../Context";
         <p className="text-sm text-gray-500">Size: Large</p>
         <p className="text-sm text-gray-500">Color: White</p>
         <div className="flex justify-between items-center mt-2">
-          <p className="font-bold">$145</p>
+          <p className="font-bold">$ {item.price} </p>
           <div className="flex items-center space-x-2">
-            <button className="w-6 h-6 bg-gray-100 rounded flex items-center justify-center">
+            <button 
+            onClick={() => decreaseItem(item.id)}
+            className="w-6 h-6 bg-gray-100 rounded flex items-center justify-center">
               −
             </button>
-            <span className="text-sm">1</span>
-            <button className="w-6 h-6 bg-gray-100 rounded flex items-center justify-center">
+            <span className="text-sm">{item.quantity} </span>
+            <button 
+            onClick={() => increaseItem(item.id)}
+            className="w-6 h-6 bg-gray-100 rounded flex items-center justify-center">
               +
             </button>
           </div>
--- src/Context/CartProvider.jsx
+++ src/Context/CartProvider.jsx
@@ -7,12 +7,14 @@ export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const [products, setProducts] = useState(initialProducts);
 
+  
+
   //  Add item to cart
   const addToCart = (product) => {
     if (product.stock === 0 || isInCart(product.id)) {
       return;
     }
-    setCartItems((prevItems) => [...prevItems, product]);
+    setCartItems((prevItems) => [...prevItems, { ...product, quantity: 1 }]);
 
     setProducts((prevProducts) =>
       prevProducts.map((p) =>
@@ -23,6 +25,9 @@ export const CartProvider = ({ children }) => {
 
   // remove item to cart
   const removeFromCart = (productId) => {
+     const itemToRemove = cartItems.find((item) => item.id === productId);
+     if (!itemToRemove) return;
+
     setCartItems((prevItems) =>
       prevItems.filter((item) => item.id !== productId)
     );
@@ -30,6 +35,56 @@ export const CartProvider = ({ children }) => {
     setProducts((prevProducts) =>
       prevProducts.map((product) =>
         product.id === productId
+          ? { ...product, stock: product.stock + itemToRemove.quantity }
+          : product
+      )
+    );
+  };
+  // increate the cart item 
+  const increaseItem = (itemId) => {
+     const itemInCart = cartItems.find((item) => item.id === itemId);
+    const product = products.find((p) => p.id === itemId);
+
+    if (!itemInCart || !product || itemInCart.quantity >= product.stock) return;
+
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId 
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
+
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === itemId 
+          ? { ...product, stock: product.stock - 1 }
+          : product
+      )
+    );
+  };
+  // decrease the cart item 
+  const decreaseItem = (itemId) => {
+    const itemInCart = cartItems.find((item) => item.id === itemId);
+
+     if (!itemInCart) return;
+
+     if (itemInCart.quantity === 1) {
+      // remove item if quantity goes below 1
+      removeFromCart(itemId);
+    } else {
+      setCartItems((prevItems) =>
+        prevItems.map((item) =>
+          item.id === itemId
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+      );
+    }
+
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === itemId 
           ? { ...product, stock: product.stock + 1 }
           : product
       )
@@ -41,7 +96,15 @@ export const CartProvider = ({ children }) => {
 
   return (
     <CartContext.Provider
-      value={{ products, cartItems, isInCart, addToCart, removeFromCart }}
+      value={{
+        products,
+        cartItems,
+        isInCart,
+        addToCart,
+        removeFromCart,
+        increaseItem,
+        decreaseItem,
+      }}
     >
       {children}
     </CartContext.Provider>
