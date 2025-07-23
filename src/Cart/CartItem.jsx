e0380593f3b5d6c628f412e8ee7c9eb4caeeaa2d
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-23 22:11:01 +0600

    remove item from cart by clicking item button cross

--- src/Cart/CartItem.jsx
+++ src/Cart/CartItem.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { getImageUrl } from "../Utils/utility";
+import { CartContext } from "../Context";
 
  const CartItem = ({item}) => {
+  const {removeFromCart} = useContext(CartContext)
   return (
     <div className="flex items-start space-x-4 pb-4 border-b border-gray-200 mb-4">
       <div className="w-16 h-16 bg-gray-100 rounded flex-shrink-0 flex items-center justify-center">
@@ -14,7 +16,9 @@ import { getImageUrl } from "../Utils/utility";
       <div className="flex-grow">
         <div className="flex justify-between">
           <h3 className="font-medium">{item.title} </h3>
-          <span className="text-red-500 text-sm">×</span>
+          <button
+           onClick={() => removeFromCart(item.id)}
+           className="text-red-500 text-xl">×</button>
         </div>
         <p className="text-sm text-gray-500">Size: Large</p>
         <p className="text-sm text-gray-500">Color: White</p>
